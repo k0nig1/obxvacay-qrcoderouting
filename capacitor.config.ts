fe5d010f60025aa9cde103bbb589f8bd0716d8e3
Daniel Holterhaus <daniel.holterhaus@gmail.com>
2025-01-16 10:59:42 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'obxvacay-qrcoderouting',
+  webDir: 'dist'
+};
+
+export default config;
